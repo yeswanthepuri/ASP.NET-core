cd9f1f46d493823c00e2a6321ee650d7b8325c66
Yeswanth^k <epuri.yeshu@gmail.com>
2019-12-27 06:13:14 +0530

    register new user using identity

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployManagment.core.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewModel/RegisterViewmodel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployManagment.core.ViewModel
+{
+    public class RegisterViewmodel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [Display(Name ="Confirm Password")]
+        [Compare("Password",ErrorMessage ="Password and Confirm Passowrd Didn't match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
