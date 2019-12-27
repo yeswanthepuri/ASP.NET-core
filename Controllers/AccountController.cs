66e7ee4fb7802b85f674f2403c408570ce9b11a3
Yeswanth^k <epuri.yeshu@gmail.com>
2019-12-27 06:32:01 +0530

    ASPnet Identity usermanager and signin manager

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,17 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployManagment.core.ViewModel;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployManagment.core.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+                                 SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
         [AllowAnonymous]
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewmodel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    Email = model.Email,
+                    UserName = model.Email
+                };
+                var resultUser = await userManager.CreateAsync(user, model.Password);
+                if (resultUser.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var item in resultUser.Errors)
+                    {
+                        ModelState.AddModelError("",item.Description);
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }
