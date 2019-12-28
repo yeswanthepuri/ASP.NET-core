2614eda9b36c3cbbf063a612020d6d954ac2e677
Yeswanth^k <epuri.yeshu@gmail.com>
2019-12-28 16:29:33 +0530

    79  Get list of roles in asp.net core and 80 Edit role in asp.net core

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployManagment.core.Models;
 using EmployManagment.core.ViewModel;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -11,10 +12,13 @@ namespace EmployManagment.core.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager,
+                                        UserManager<ApplicationUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
         [HttpGet]
         public IActionResult CreateRole()
@@ -31,7 +35,7 @@ namespace EmployManagment.core.Controllers
                 var result = await roleManager.CreateAsync(role);
                 if (result.Succeeded)
                 {
-                  return  RedirectToAction("ListofUserRoles", "Administration");
+                    return RedirectToAction("ListofUserRoles", "Administration");
                 }
                 else
                 {
@@ -49,6 +53,59 @@ namespace EmployManagment.core.Controllers
         {
             var roles = roleManager.Roles;
             return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var roles = await roleManager.FindByIdAsync(id);
+            if (roles == null)
+            {
+                return View("RecordNotFound");
+            }
+            var model = new EditRoleViewModel()
+            {
+                ID = roles.Id,
+                RoleName = roles.Name
+            };
+
+            foreach (var user in userManager.Users)
+            {
+                if (await userManager.IsInRoleAsync(user, roles.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.ID);
+            if (role == null)
+            {
+                return View("RecordNotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+
+                // Update the Role using UpdateAsync
+                var result = await roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListofUserRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model);
+            }
         }
     }
 }
---
+++ ViewModel/EditRoleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployManagment.core.ViewModel
+{
+    public class EditRoleViewModel
+    {
+        public string ID { get; set; }
+        [Required(ErrorMessage = "Role Name is Required  ")]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+
+        public List<string> Users { get; set; } = new List<string>();
+    }
+}
